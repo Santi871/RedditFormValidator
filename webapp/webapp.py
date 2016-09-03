10d7674fa1c7b13ad93ba1400edfb1c067c2e230
Santi871 <santivegega@gmail.com>
2016-09-03 12:31:06 -0300

    decline catching

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -6,22 +6,30 @@ import datetime
 REDDIT_CLIENT_ID = utils.get_token("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = utils.get_token("REDDIT_CLIENT_SECRET")
 REDIRECT_URI = utils.get_token("REDIRECT_URI")
-FORM_PREFILLED_URL_TEMPLATE = utils.get_token("FORM_PREFILLED_URL_TEMPLATE", section='form')
 
 app = Flask(__name__, static_url_path='')
-r = praw.Reddit("windows:Google Form validator (access form with Reddit identity) v0.1 - by /u/Santi871")
+r = praw.Reddit("windows:Google Form validator (access form with Reddit identity) v0.2 - by /u/Santi871")
 r.set_oauth_app_info(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDIRECT_URI)
+ip_form_ids = dict()
 
 
 @app.route('/auth')
 def oauth():
     url = r.get_authorize_url('uniqueKey')
+    form_id = request.args.get('form_id')
+    field_id = request.args.get('field_id')
+    ip_form_ids[request.remote_addr] = form_id + "," + field_id
+
     return redirect(url, code=302)
 
 
 @app.route('/authorize_callback')
 def authorized():
     code = request.args.get('code', ' ')
+    split_ids = ip_form_ids[request.remote_addr].split(',')
+    ip_form_ids.pop(request.remote_addr, None)
+    form_id = split_ids[0]
+    field_id = split_ids[1]
 
     try:
         r.get_access_information(code)
@@ -35,5 +43,6 @@ def authorized():
     with open("authorized_users", "a+") as text_file:
         text_file.write(name + "    " + now.strftime("%Y-%m-%d %H:%M") + '\n')
 
-    url = FORM_PREFILLED_URL_TEMPLATE + name
+    url = "https://docs.google.com/forms/d/e/" + form_id + "/viewform?entry." + field_id + "=" + name
+
     return redirect(url, code=302)
